9011856b7d8597793f5eeca6695e74bd835ec89e
Adam Zambrzycki <adam@zambrzycki.net>
2025-05-23 10:27:25 +0200

    Scroll to top on navigation

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,27 @@
-import {StrictMode} from 'react'
+import {StrictMode, useEffect} from 'react'
 import {createRoot} from 'react-dom/client'
 import './common.css'
-import {BrowserRouter, Route, Routes} from "react-router";
+import {BrowserRouter, Route, Routes, useLocation} from "react-router";
 import Index from "./pages/player";
 import {GameDetailsPage} from "./pages/game";
 import App from "./pages/main";
 import About from "./pages/about";
 import './i18n';
 
+function ScrollToTop() {
+    const { pathname } = useLocation();
+
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [pathname]);
+
+    return null;
+}
+
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <BrowserRouter>
+            <ScrollToTop/>
             <Routes>
                 <Route path="/" element={<App/>}/>
                 <Route path="/about" element={<About/>}/>
