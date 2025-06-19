fbbd4847a8ea41f009ec71ad85e3b002e0da01c5
Adam Zambrzycki <adam@zambrzycki.net>
2025-06-19 19:10:44 +0200

    Fill about page

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -12,6 +12,10 @@ const Background = styled.div`
 const Message = styled.div`
     padding: 20px;
     text-align: left;
+    
+    small {
+        color: #ccc;
+    }
 `;
 
 const Logo = () => (
@@ -45,6 +49,30 @@ function About() {
                     </Link>
                 </div>
                 <Box>
+                    <h3 style={{marginTop: 0}}>Why this website exists</h3>
+                    It seems that every Reksio page has to cease to exist at some point. The first one was removed by Google. The second one first lost its domain (expired) to online casino, and later got completely removed by Odoo.
+                    I was able to create a new Odoo account with the old name, so that it is redirecting to this new website.
+                    <br/>
+                    <br/>
+                    This website aims not to share the fate of its predecessors:
+                    <ul>
+                        <li>The domain is registered for 9 years in advance <small>(until 2035-02-05)</small></li>
+                        <li>It's hosted on Github Pages <small>(more stable than Blogger and Odoo like services)</small></li>
+                        <li>Relies on it's own files hosting + archive.org mirrors <small>(instead of free cloud drives known to go down)</small></li>
+                        <li>Admin is permanently online <small>(no contact with the admin was a recurring theme.)</small></li>
+                        <li>Can be modified by other people than admin <small>(requires two trusted pull-request approvals)</small></li>
+                        <li>It's <a href="https://github.com/ZagrajWReksia/ZagrajWReksia.github.io">open source</a></li>
+                    </ul>
+                </Box>
+                <Box style={{marginTop: '10px'}}>
+                    <h3 style={{marginTop: 0}}>ReksioEngine</h3>
+                    In addition to offering downloads like on the old websites, this website is also giving you the possibility of playing Reksio games entirely in web browser.
+                    Which is all made possible thanks to my project <a href="https://github.com/ReksioEngine/ReksioEngine">ReksioEngine</a>.
+                    <br/>
+                    <br/>
+                    <i>"This project is an effort to reimplement the BlooMoo and Piklib engines, both used in the "Przygody Reksia" video game series. The goal is to enable the game to run directly in a web browser, utilizing its original assets and scripts. In addition to making the game accessible on modern platforms such as web browsers and mobile devices, the project also contributes to documenting and preserving the game engine and its custom scripting language, helping to understand and archive the inner workings of the technology behind the game."</i>
+                </Box>
+                <Box style={{marginTop: '10px'}}>
                     <h3 style={{marginTop: 0}}>Credits</h3>
                     <div><strong>Website:</strong></div>
                     <div>Code: Adikso</div>
