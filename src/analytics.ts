50d19406d504c81373817f6f84a1840a08d45f48
Adam Zambrzycki <adam@zambrzycki.net>
2025-07-23 19:18:10 +0200

    Add download event name

--- src/analytics.ts
+++ src/analytics.ts
@@ -16,6 +16,7 @@ export const trackUrl = (url: string, metadata?: any) => {
         // eslint-disable-next-line @typescript-eslint/ban-ts-comment
         // @ts-expect-error
         window.umami.track({
+            "name": "Download",
             "data": metadata,
             "website": "884a9f2d-33cc-4afc-89aa-2996306a2248",
             "title": "Download",
