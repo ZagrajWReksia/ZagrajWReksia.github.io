dd0e2261b2464cc94a553b62eac2350673573ac2
Adam Zambrzycki <adam@zambrzycki.net>
2026-02-07 00:08:44 +0100

    Remove unused code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,3 @@
-import type { Metadata } from 'next';
-
-export const metadata: Metadata = {
-  title: 'Zagraj w Reksia',
-  description: "Download and play games from the original Reksio's Adventures (Przygody Reksia) series",
-  openGraph: {
-    title: 'zagrajwreksia.pl',
-    type: 'website',
-    images: ['https://zagrajwreksia.pl/logo.png'],
-    description: "Download and play games from the original Reksio's Adventures (Przygody Reksia) series",
-  },
-  manifest: '/site.webmanifest',
-};
-
 export default function RootLayout({
   children,
 }: {
